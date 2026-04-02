8c27629524c61df39c46286a71f9ea4cd35f5ec0
Kessential <minhk3808@gmail.com>
2026-04-02 20:55:27 +0700

    2/4 3

--- ld.cpp
+++ ld.cpp
@@ -1,32 +1,51 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 double phi(double x) { return 17 / (pow(x, 4) + 25); }
 
 void solve(double x_0, double epsilon, double q) {
-  if (q >= 1)
+  if (q >= 1 || q <= 0) {
+    cout << "He so co q phai nho hon 1!\n";
     return;
+  }
+
   double epsilon_0 = (1 - q) * epsilon / q;
   double x_old = x_0, x_new;
   int n = 0;
-  cout << fixed << setprecision(8) << setw(3) << "k" << setw(15) << "x_k"
+
+  cout << fixed << setprecision(8);
+  cout << setw(3) << "k" << setw(15) << "x_k"
        << setw(15) << "x_k+1" << setw(15) << "Sai so" << "\n";
+  cout << "-------------------------------------------------\n";
+
   while (true) {
     ++n;
     x_new = phi(x_old);
+
     double error = abs(x_old - x_new);
-    cout << setw(3) << n << setw(15) << x_old << setw(15) << x_new << setw(15)
-         << error << "\n";
+
+    cout << setw(3) << n 
+         << setw(15) << x_old 
+         << setw(15) << x_new 
+         << setw(15) << error << "\n";
+
     if (error < epsilon_0) {
+      cout << "-------------------------------------------------\n";
       cout << "Nghiem gan dung cua PT la: " << x_new << "\n";
       break;
     }
     x_old = x_new;
-    if (n > 1000)
+
+    if (n > 1000) {
+      cout << "Thuat toan ko hoi tu sau 1000 lan lap\n";
       break;
-  }
+    }
+   }
 }
+
 int main() {
   double x_0, epsilon, q;
   cin >> x_0 >> epsilon >> q;
--- newton_raphson_tn.cpp
+++ newton_raphson_tn.cpp
@@ -1,3 +1,7 @@
+// Phương pháp tiếp tuyến (Newton - Raphson) 
+// theo công thức sai số mục tiêu (tiên nghiệm)
+// | x_k - x* | <= | f(x_k) | / m1
+
 #include <cmath>
 #include <iomanip>
 #include <iostream>
@@ -10,35 +14,47 @@ double d2f(double x) { return exp(x) + cos(x / 2); }
 
 void solve(double a, double b, double epsilon) {
   double x_old;
+
   if (f(a) * d2f(a) > 0) {
     x_old = a;
   } else {
     x_old = b;
   }
+
   double m1 = min(abs(df(a)), abs(df(b)));
 
   cout << fixed << setprecision(10);
   cout << "Diem bat dau x0 = " << x_old << "\n";
   cout << "----------------------------------------------------" << "\n";
-  cout << setw(3) << "k" << setw(15) << "x_k" << setw(15) << "x_k+1" << setw(15)
-       << "Sai so"
-       << "\n";
+  cout << setw(3) << "k" 
+       << setw(15) << "x_k"
+       << setw(15) << "x_k+1"
+       << setw(15) << "Sai so\n";
 
   int k = 1;
+
   while (true) {
     double x_new = x_old - f(x_old) / df(x_old);
     double error = abs(f(x_new)) / m1;
-    cout << setw(3) << k << setw(15) << x_old << setw(15) << x_new << setw(15)
-         << error << "\n";
+
+    cout << setw(3) << k
+         << setw(15) << x_old
+         << setw(15) << x_new
+         << setw(15) << error << "\n";
+
     if (error <= epsilon) {
       cout << "----------------------------------------------------\n";
       cout << "Nghiem xap xi tim duoc: " << x_new << "\n";
       break;
     }
+
     x_old = x_new;
     k++;
-    if (k > 100)
+
+    if (k > 100) {
+      cout << "Thuat toan chay hon 100 lan lap!\n";
       break;
+    }
   }
 }
 
